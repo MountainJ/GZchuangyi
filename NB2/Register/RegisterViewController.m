e189e7c1887cee87d60516a3fc5933329cf2026d
jay <ZHANGYI540@pingan.com.cn>
2018-12-16 15:39:35 +0800

    修改注册界面

--- NB2/Register/RegisterViewController.m
+++ NB2/Register/RegisterViewController.m
@@ -8,6 +8,10 @@
 
 #import "RegisterViewController.h"
 #import "ResgitViewController.h"
+
+#define LINE_COLOR  [[UIColor lightGrayColor] colorWithAlphaComponent:0.5]
+
+
 @implementation RegisterViewController
 {
     UITextField *nameTF;
@@ -55,41 +59,21 @@
     // 用户名输入
     nameTF = [[UITextField alloc] initWithFrame:CGRectMake(SCREEN_WIDTH*0.15, CGRectGetMaxY(imgview.frame)+50,SCREEN_WIDTH*0.7, 40)];
     [nameTF setPlaceholder:@" 请输入手机号"];
-    [nameTF setValue:[UIColor whiteColor] forKeyPath:@"_placeholderLabel.textColor"];
-    //    [nameTF addTarget:self action:@selector(returnResignKeyBoard:) forControlEvents:UIControlEventEditingDidEndOnExit];
-    nameTF.clearButtonMode = UITextFieldViewModeWhileEditing;//清除button
-    nameTF.autocapitalizationType = UITextAutocapitalizationTypeNone;//取消自动大小写
-    nameTF.contentVerticalAlignment =UIControlContentVerticalAlignmentCenter;
-    //nameTF.delegate = self;
-    nameTF.textColor=[UIColor whiteColor];
-    nameTF.layer.masksToBounds=YES;
-    nameTF.layer.cornerRadius=3;
-    nameTF.backgroundColor=[UIColor grayColor];
-    [nameTF setFont:[UIFont systemFontOfSize:15]];
+    [self changeLayerCornerLeftImg:nameTF ];
     [bodyView addSubview:nameTF];
     
-    // 密码输入
+    // 验证码输入
     keywordNoTF = [[UITextField alloc] initWithFrame:CGRectMake(SCREEN_WIDTH*0.15, CGRectGetMaxY(nameTF.frame)+20, SCREEN_WIDTH*0.35, 40)];
     [keywordNoTF setPlaceholder:@" 请输入验证码"];
-    keywordNoTF.secureTextEntry = YES;
-    keywordNoTF.clearButtonMode = UITextFieldViewModeWhileEditing;//清除button
-    [keywordNoTF setValue:[UIColor whiteColor] forKeyPath:@"_placeholderLabel.textColor"];
-    keywordNoTF.layer.masksToBounds=YES;
-    keywordNoTF.layer.cornerRadius=3;
-    keywordNoTF.textColor=[UIColor whiteColor];
-    keywordNoTF.backgroundColor=[UIColor grayColor];
-    
-    keywordNoTF.autocapitalizationType = UITextAutocapitalizationTypeNone;//取消自动大小写
-    keywordNoTF.contentVerticalAlignment =UIControlContentVerticalAlignmentCenter;
-    //keywordNoTF.delegate = self;
-    [keywordNoTF setFont:[UIFont systemFontOfSize:15]];
+  
+    [self changeLayerCornerLeftImg:keywordNoTF];
     [bodyView addSubview:keywordNoTF];
     
     lognA = [[UIButton alloc] initWithFrame:CGRectMake(SCREEN_WIDTH*0.55 ,CGRectGetMaxY(nameTF.frame)+20,SCREEN_WIDTH*0.3,40)];
     //    [lognB setBackgroundImage:[UIImage imageNamed:@"btn_big_n"] forState:UIControlStateNormal];
     lognA.backgroundColor = [UIColor colorWithRed:4.0/255 green:145.0/255 blue:218.0/255 alpha:1];
     lognA.layer.masksToBounds=YES;
-    lognA.layer.cornerRadius=3;
+    lognA.layer.cornerRadius=5;
     lognA.showsTouchWhenHighlighted = YES;
     [lognA setTitle:@"发送验证码" forState:UIControlStateNormal];
     [lognA.titleLabel setFont:[UIFont systemFontOfSize:15]];
@@ -97,23 +81,44 @@
     [bodyView addSubview:lognA];
     
     UIButton *lognB = [[UIButton alloc] initWithFrame:CGRectMake(SCREEN_WIDTH*0.15 ,CGRectGetMaxY(keywordNoTF.frame)+20,SCREEN_WIDTH*0.7,40)];
-    //    [lognB setBackgroundImage:[UIImage imageNamed:@"btn_big_n"] forState:UIControlStateNormal];
     lognB.backgroundColor = [UIColor colorWithRed:4.0/255 green:145.0/255 blue:218.0/255 alpha:1];
     lognB.layer.masksToBounds=YES;
-    lognB.layer.cornerRadius=3;
+    lognB.layer.cornerRadius=5;
     lognB.showsTouchWhenHighlighted = YES;
     [lognB setTitle:@"提交注册" forState:UIControlStateNormal];
     [lognB.titleLabel setFont:[UIFont systemFontOfSize:20]];
     [lognB addTarget:self action:@selector(registUser) forControlEvents:UIControlEventTouchUpInside];
     [bodyView addSubview:lognB];
     
-    
 }
-//- (void) handleTimer: (NSTimer *) timer
-//{
-//    [timer setFireDate:[NSDate distantFuture]];
-//    [lognA setEnabled:YES];
-//}
+
+
+- (void)changeLayerCornerLeftImg:(UITextField *)textField
+{
+    textField.autocapitalizationType = UITextAutocapitalizationTypeNone;//取消自动大小写
+    textField.contentVerticalAlignment =UIControlContentVerticalAlignmentCenter;
+    textField.clearButtonMode = UITextFieldViewModeWhileEditing;//清除button
+    
+    textField.textColor=[UIColor darkTextColor];
+    [textField setFont:[UIFont systemFontOfSize:15]];
+    
+    textField.backgroundColor= [UIColor whiteColor];
+    [textField setValue:LINE_COLOR forKeyPath:@"_placeholderLabel.textColor"];
+    
+    textField.layer.cornerRadius = 5.f;
+    textField.layer.masksToBounds = YES;
+    textField.layer.borderWidth = 1.f;
+    textField.layer.borderColor =LINE_COLOR.CGColor;
+    
+    UIImageView *imgView = [[UIImageView alloc] init];
+    imgView.backgroundColor = [UIColor clearColor];
+    imgView.frame = CGRectMake(0, 0, 10., 30.);
+    textField.leftViewMode = UITextFieldViewModeAlways;
+    textField.leftView = imgView;
+}
+
+
+
 -(void)sendCode
 {
     if ([nameTF.text length]!=11||[[nameTF.text substringWithRange:NSMakeRange(0, 1)] integerValue]!=1)
